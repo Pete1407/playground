8e0f1712eff33ca9642fb682f16d0d5430e3ca09
Pittawat <pete.pittawas@gmail.com>
2024-12-08 22:52:20 +0700

    add comment round two

--- app/src/main/java/com/example/playgroundspace/MainActivity.kt
+++ app/src/main/java/com/example/playgroundspace/MainActivity.kt
@@ -26,6 +26,7 @@ class MainActivity : ComponentActivity() {
 //                        modifier = Modifier.padding(innerPadding)
 //                    )
 
+                    // for only test
                     Column(modifier = Modifier.padding(innerPadding)) {
                         Text(
                             text = "First Text"

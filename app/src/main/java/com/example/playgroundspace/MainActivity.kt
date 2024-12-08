50f5b1f8f2b895367de2d4e0b827eacfe5ee9416
Pittawat <pete.pittawas@gmail.com>
2024-12-08 22:51:25 +0700

    add code round 02

--- app/src/main/java/com/example/playgroundspace/MainActivity.kt
+++ app/src/main/java/com/example/playgroundspace/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -24,6 +25,20 @@ class MainActivity : ComponentActivity() {
 //                        name = "Android",
 //                        modifier = Modifier.padding(innerPadding)
 //                    )
+
+                    Column(modifier = Modifier.padding(innerPadding)) {
+                        Text(
+                            text = "First Text"
+                        )
+
+                        Text(
+                            text = "Second Text"
+                        )
+
+                        Text(
+                            text = "Third Text"
+                        )
+                    }
                 }
             }
         }

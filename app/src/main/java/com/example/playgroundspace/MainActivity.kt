080d7dbcfa7a05dd20f3a5813e6016e4601149c2
Pittawat <pete.pittawas@gmail.com>
2024-12-08 22:49:53 +0700

    Revert "add code"
    This reverts commit 713bbbb199f51af41fb9b786d7ff05532200f9d5.

--- app/src/main/java/com/example/playgroundspace/MainActivity.kt
+++ app/src/main/java/com/example/playgroundspace/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -25,20 +24,6 @@ class MainActivity : ComponentActivity() {
 //                        name = "Android",
 //                        modifier = Modifier.padding(innerPadding)
 //                    )
-
-                    Column(modifier = Modifier.padding(innerPadding)) {
-                        Text(
-                            text = "First Text"
-                        )
-
-                        Text(
-                            text = "Second Text"
-                        )
-
-                        Text(
-                            text = "Third Text"
-                        )
-                    }
                 }
             }
         }

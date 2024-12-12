a46a5010d2a0f18b57be6ccfe7067f7650c53dca
Pittawas Prechaborrisutkun <Pittawas.Prechaborrisutkun@krungsri.com>
2024-12-12 17:37:32 +0700

    add code for show basic bottomsheet

--- app/src/main/java/com/example/playgroundspace/MainActivity.kt
+++ app/src/main/java/com/example/playgroundspace/MainActivity.kt
@@ -8,30 +8,43 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Create
 import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -50,9 +63,11 @@ import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
@@ -82,6 +97,9 @@ class MainActivity : ComponentActivity() {
 
                         //TestSideEffect()
                     }
+                },
+                floatingActionButton = {
+                    SectionMyBottomSheetDialog()
                 }
             )
         }
@@ -274,7 +292,6 @@ fun CustomTextFieldWithButton(
     }
 }
 
-@Preview
 @Composable
 fun SectionAlertDialog() {
     var openAlertDialog by remember {
@@ -289,7 +306,7 @@ fun SectionAlertDialog() {
         modifier = Modifier.padding(PaddingTop)
     ) {
         Text(
-            "Show AlertDialog"
+            stringResource(R.string.button_text_alertdialog)
         )
         when {
             openAlertDialog -> {
@@ -380,3 +397,64 @@ fun TestSideEffect(){
         secondProveValue = false
     }
 }
+
+@Composable
+fun SectionMyBottomSheetDialog(){
+    var showBottomSheet by remember {
+        mutableStateOf(false)
+    }
+    Button(onClick = {
+        showBottomSheet = true
+    }) {
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_foreground),
+            contentDescription = "",
+            modifier = Modifier.size(50.dp)
+        )
+
+        if(showBottomSheet){
+            MyBottomSheetDialog(
+                onDismissRequest = {
+                    showBottomSheet = false
+                }
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyBottomSheetDialog(
+    onDismissRequest : () -> Unit
+){
+    val sheetState = rememberModalBottomSheetState()
+
+    ModalBottomSheet(
+        onDismissRequest = {
+            onDismissRequest.invoke()
+        },
+        sheetState = sheetState,
+    ){
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .windowInsetsPadding(WindowInsets.safeDrawing),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ){
+            Text("BottomSheet", fontStyle = FontStyle.Italic, fontSize = 30.sp)
+            ShowOrderList()
+        }
+    }
+}
+
+@Composable
+fun ShowOrderList(){
+    LazyColumn {
+        items(orderList){ item ->
+            Text(text = item)
+        }
+    }
+}
+
+val orderList = listOf<String>("First","Second","third","Fourth","Fifth","Sixth","Seventh")
